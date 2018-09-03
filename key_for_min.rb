cca5000bf6cefee5d65bd3ecacdc3f7b502aa8e3
ALEKSANDAR VUKASINOVIC <alekvuka@gmail.com>
2018-09-03 22:44:27 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -14,7 +14,10 @@ def key_for_min_value(name_hash)
     end
     temp_array = temp_array.sort
     smallest_value = temp_array[0]
-    return smallest_value
-  end 
+
+    name_hash.each do |key, value|
+      if value == smallest_value
+        return key 
+  end
 
 end
