b4b82599468aa4c32a9515cbd4f11a5e2f89389a
ALEKSANDAR VUKASINOVIC <alekvuka@gmail.com>
2018-09-03 22:38:55 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,15 @@
 
 def key_for_min_value(name_hash)
 
+  temp_array = Array.new
+  i = 0
+  name_hash.each do |key, value|
+    temp_array[i] = value
+    i += 1
+  end
+  temp_array.sort
+  smallest_value = temp_array[0]
+  return smallest_value 
+
+
 end
